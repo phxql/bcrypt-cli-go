376d48a4ffea09d25ac30397e0de65a644b0fb03
Moritz Kammerer <kammerer.moritz@gmail.com>
2018-08-13 12:20:47 +0200

    Initial version

---
+++ bcrypt-cli.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+import (
+	"golang.org/x/crypto/bcrypt"
+	"os"
+	"strconv"
+	"bufio"
+)
+
+func main() {
+    var password = "-"
+	var cost = 12
+
+	// Use password from args, if provided
+	if len(os.Args) > 1 {
+		password = os.Args[1]
+	}
+
+	// Use cost from args, if provided
+	if len(os.Args) > 2 {
+		cost = convertStringToInt(os.Args[2])
+	}
+
+	if password == "-" {
+		password = readFromStdin()
+	}
+
+	var hash = bcryptPassword(password, cost)
+	fmt.Println(hash)
+}
+
+func bcryptPassword(password string, cost int) string {
+	var hashedPassword, err = bcrypt.GenerateFromPassword([]byte(password), cost)
+	if err != nil {
+		panic(err)
+	}
+	return string(hashedPassword)
+}
+
+func convertStringToInt(input string) int {
+	var i, err = strconv.Atoi(input)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func readFromStdin() string {
+	var reader = bufio.NewReader(os.Stdin)
+	fmt.Print("Enter password: ")
+	var input, err = reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+
+	return input
+}
